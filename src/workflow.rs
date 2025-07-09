db3fe583e32b37ea59c0f1e8350b95d49d5f21cc
Ethan Wolinsky <ethan@alphataraxia.com>
2025-07-08 21:54:03 -0400

    Add workflow interface

---
+++ src/workflow.rs
@@ -0,0 +1,21 @@
+use crate::HatchetClient;
+use crate::HatchetError;
+use serde::Serialize;
+
+pub struct Workflow<'a> {
+    name: &'a str,
+    client: &'a HatchetClient,
+}
+
+impl<'a> Workflow<'a> {
+    pub fn new(name: &'a str, client: &'a HatchetClient) -> Self {
+        Self { name, client }
+    }
+
+    pub async fn run_no_wait<T>(&self, input: T) -> Result<String, HatchetError>
+    where
+        T: Serialize,
+    {
+        self.client.run_no_wait(self.name, input).await
+    }
+}
