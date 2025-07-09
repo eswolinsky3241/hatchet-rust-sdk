754df998743c86c107f114176685dc36b4108c4b
Ethan Wolinsky <ethan@alphataraxia.com>
2025-07-08 21:53:17 -0400

    Add config from environment

---
+++ src/config.rs
@@ -0,0 +1,53 @@
+use base64::Engine;
+use base64::engine::general_purpose::URL_SAFE_NO_PAD;
+use std::env;
+
+#[derive(Debug)]
+pub struct HatchetConfig {
+    pub api_token: String,
+    pub grpc_broadcast_address: String,
+    pub server_url: String,
+}
+
+impl HatchetConfig {
+    pub fn from_env() -> Result<Self, HatchetConfigError> {
+        let token = env::var("HATCHET_CLIENT_TOKEN").map_err(HatchetConfigError::MissingEnvVar)?;
+        let parts: Vec<&str> = token.split('.').collect();
+        println!("Token parts: {:?}", parts);
+        if parts.len() < 2 {
+            return Err(HatchetConfigError::InvalidTokenFormat);
+        }
+
+        let payload_bytes = URL_SAFE_NO_PAD
+            .decode(parts[1])
+            .map_err(HatchetConfigError::Base64Decode)?;
+        let payload_json: serde_json::Value =
+            serde_json::from_slice(&payload_bytes).map_err(HatchetConfigError::JsonDecode)?;
+
+        let grpc_broadcast_address = payload_json
+            .get("grpc_broadcast_address")
+            .and_then(|v| v.as_str())
+            .ok_or(HatchetConfigError::MissingGrpcAddress)?;
+
+        let server_url = payload_json
+            .get("server_url")
+            .and_then(|v| v.as_str())
+            .ok_or(HatchetConfigError::MissingServerUrl)?;
+
+        Ok(Self {
+            api_token: token,
+            grpc_broadcast_address: grpc_broadcast_address.to_string(),
+            server_url: server_url.to_string(),
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum HatchetConfigError {
+    MissingEnvVar(std::env::VarError),
+    InvalidTokenFormat,
+    Base64Decode(base64::DecodeError),
+    JsonDecode(serde_json::Error),
+    MissingGrpcAddress,
+    MissingServerUrl,
+}
