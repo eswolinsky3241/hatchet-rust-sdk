8f9370ec7f063daf7feedaa4d5983e92487545b3
Ethan Wolinsky <ethan@alphataraxia.com>
2025-07-13 14:14:03 -0400

    Add additional models

--- src/models.rs
+++ src/models.rs
@@ -5,13 +5,35 @@ use serde_json::Value;
 pub struct GetWorkflowRunResponse {
     pub tasks: Vec<Task>,
     pub run: Run,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Task {
-    pub status: String,
+    #[serde(rename = "errorMessage")]
+    pub error_message: String,
     pub output: Option<Value>,
 }
 
 #[derive(Debug, Deserialize)]
-pub struct Run {}
+pub struct Task {
+    pub status: WorkflowStatus,
+    #[serde(rename = "errorMessage")]
+    pub error_message: String,
+    pub output: Option<Value>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Run {
+    pub status: WorkflowStatus,
+    #[serde(rename = "errorMessage")]
+    pub error_message: String,
+    pub output: Option<Value>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum WorkflowStatus {
+    Running,
+    Failed,
+    Completed,
+    Queued,
+    Cancelled,
+    #[serde(other)]
+    Unknown,
+}
