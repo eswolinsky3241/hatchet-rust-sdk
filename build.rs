01bd7bf8716204d8ac263ca990fca88b7db841db
Ethan Wolinsky <ethan@alphataraxia.com>
2025-07-15 21:17:05 -0400

    Add dispatcher protos

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("api-contracts/protos/workflows.proto")?;
+    tonic_build::configure().compile_protos(
+        &[
+            "api-contracts/protos/workflows.proto",
+            "api-contracts/protos/dispatcher.proto",
+        ],
+        &["api-contracts/protos"],
+    )?;
     Ok(())
 }
