39b7b9ff3711218de46e2aa92f54c0977f870e33
Ethan Wolinsky <ethan@alphataraxia.com>
2025-07-27 21:04:10 -0400

    Implement put_log method

---
+++ src/clients/event_client.rs
@@ -0,0 +1,38 @@
+use std::sync::Arc;
+
+use crate::HatchetClient;
+use crate::grpc::events;
+use crate::grpc::events::PutLogRequest;
+use crate::utils::proto_timestamp_now;
+
+pub struct EventClient {
+    client: Arc<HatchetClient>,
+}
+
+impl EventClient {
+    pub async fn new(client: Arc<HatchetClient>) -> Self {
+        Self { client: client }
+    }
+    pub async fn put_log(
+        &self,
+        step_run_id: &str,
+        message: String,
+    ) -> Result<(), crate::HatchetError> {
+        let request = tonic::Request::new(PutLogRequest {
+            step_run_id: step_run_id.to_string(),
+            created_at: Some(proto_timestamp_now()?),
+            message: message,
+            level: None,
+            metadata: String::from(""),
+        });
+
+        let _ = self
+            .client
+            .grpc_unary(request, |channel, request| async move {
+                let mut client = events::events_service_client::EventsServiceClient::new(channel);
+                client.put_log(request).await
+            })
+            .await?;
+        Ok(())
+    }
+}
