3669f22d857c941d61c21224ee315a48b648fe67
Ethan Wolinsky <ethan@alphataraxia.com>
2025-07-27 22:12:13 -0400

    Send logs in separate thread for performance

--- src/tasks/context.rs
+++ src/tasks/context.rs
@@ -1,25 +1,31 @@
 use std::sync::Arc;
 
+use tokio::sync::mpsc;
+
 use crate::clients::EventClient;
 use crate::{HatchetClient, HatchetError};
 pub struct Context {
-    event_client: Arc<EventClient>,
-    step_run_id: String,
+    logger_tx: mpsc::Sender<String>,
 }
 
 impl Context {
     pub fn new(client: Arc<HatchetClient>, step_run_id: &str) -> Self {
         let event_client = Arc::new(EventClient::new(client.clone()));
-        Self {
-            event_client,
-            step_run_id: step_run_id.to_string(),
-        }
+        let (tx, mut rx) = mpsc::channel::<String>(100);
+        let step_run_id = step_run_id.to_string();
+        tokio::spawn(async move {
+            while let Some(message) = rx.recv().await {
+                event_client.put_log(&step_run_id, message).await.unwrap();
+            }
+        });
+        Self { logger_tx: tx }
     }
 
+    pub async fn run_logger_thread(&self) -> () {}
+
     pub async fn log(&self, message: String) -> Result<(), HatchetError> {
-        self.event_client
-            .put_log(&self.step_run_id, message)
-            .await?;
+        self.logger_tx.send(message).await.unwrap();
+
         Ok(())
     }
 }
