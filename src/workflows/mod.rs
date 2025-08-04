eb3f7a9195e3200aa6660d948f67a310155b2fe5
Ethan Wolinsky <ethan@alphataraxia.com>
2025-08-03 21:32:28 -0400

    Remove old task trait

--- src/workflows/mod.rs
+++ src/workflows/mod.rs
@@ -1,9 +1,7 @@
 pub(crate) mod context;
 pub mod task;
-pub mod task_function;
 pub mod workflow;
 
 pub use context::Context;
 pub use task::Task;
-pub use task_function::TaskFunction;
 pub use workflow::{TriggerWorkflowOptions, Workflow};
