bc6560815fd35b76f484c14f66eb121a30dd7722
Ethan Wolinsky <ethan@alphataraxia.com>
2025-08-24 20:44:25 -0400

    Test workflow run method

--- tests/test_job_creation.rs
+++ tests/test_job_creation.rs
@@ -1,10 +1,11 @@
-use hatchet_sdk::EmptyModel;
 use hatchet_sdk::HatchetClient;
 use hatchet_sdk::HatchetError;
 use hatchet_sdk::Worker;
 use serde::{Deserialize, Serialize};
+use testcontainers::core::wait::HealthWaitStrategy;
 use testcontainers::{
     GenericImage, Healthcheck, ImageExt,
+    core::ContainerAsync,
     core::{ExecCommand, IntoContainerPort, WaitFor},
     runners::AsyncRunner,
 };
@@ -12,72 +13,7 @@ use tokio::io::AsyncReadExt;
 
 #[tokio::test]
 async fn test_hatchet() {
-    let postgres = GenericImage::new("postgres", "15.6")
-        .with_exposed_port(5432.tcp())
-        .with_cmd(vec!["postgres", "-c", "max_connections=200"])
-        .with_health_check(
-            Healthcheck::cmd(vec!["CMD-SHELL", "pg_isready -d hatchet -U hatchet"])
-                .with_interval(Some(std::time::Duration::from_secs(5)))
-                .with_timeout(Some(std::time::Duration::from_secs(5)))
-                .with_retries(Some(5))
-                .with_start_period(Some(std::time::Duration::from_secs(10))),
-        )
-        .with_network("hatchet")
-        .with_env_var("POSTGRES_USER", "hatchet")
-        .with_env_var("POSTGRES_PASSWORD", "hatchet")
-        .with_env_var("POSTGRES_DB", "hatchet")
-        .start()
-        .await
-        .unwrap();
-
-    let hatchet = GenericImage::new("ghcr.io/hatchet-dev/hatchet/hatchet-lite", "latest")
-        .with_wait_for(WaitFor::message_on_either_std(
-            "created tenant 707d0855-80ab-4e1f-a156-f1c4546cbf52",
-        ))
-        .with_wait_for(WaitFor::Duration {
-            length: std::time::Duration::from_secs(10),
-        })
-        .with_mapped_port(8888, 8888.tcp())
-        .with_mapped_port(7077, 7077.tcp())
-        .with_env_var(
-            "DATABASE_URL",
-            format!(
-                "postgresql://hatchet:hatchet@{}:5432/hatchet?sslmode=disable",
-                postgres.get_bridge_ip_address().await.unwrap()
-            ),
-        )
-        .with_network("hatchet")
-        .with_env_var("SERVER_AUTH_COOKIE_DOMAIN", "localhost")
-        .with_env_var("SERVER_AUTH_COOKIE_INSECURE", "t")
-        .with_env_var("SERVER_GRPC_BIND_ADDRESS", "0.0.0.0")
-        .with_env_var("SERVER_GRPC_INSECURE", "t")
-        .with_env_var("SERVER_GRPC_BROADCAST_ADDRESS", "localhost:7077")
-        .with_env_var("SERVER_GRPC_PORT", "7077")
-        .with_env_var("SERVER_URL", "http://localhost:8888")
-        .with_env_var("SERVER_AUTH_SET_EMAIL_VERIFIED", "t")
-        .with_env_var("SEVER_DEFAULT_ENGINE_VERSION", "V1")
-        .with_env_var(
-            "SERVER_INTERNAL_CLIENT_INTERNAL_GRPC_BROADCAST_ADDRESS",
-            "localhost:7077",
-        )
-        .start()
-        .await
-        .unwrap();
-
-    let command = ExecCommand::new(vec![
-        "/hatchet-admin",
-        "token",
-        "create",
-        "--config",
-        "/config",
-        "--tenant-id",
-        "707d0855-80ab-4e1f-a156-f1c4546cbf52",
-    ]);
-
-    let mut result = hatchet.exec(command).await.unwrap();
-    let mut token = String::new();
-    result.stdout().read_to_string(&mut token).await.unwrap();
-    // std::thread::sleep(std::time::Duration::from_secs(30));
+    let (_postgres, _hatchet, token) = start_containers_and_get_token().await;
     let hatchet = HatchetClient::from_token(&token.trim(), "none")
         .await
         .unwrap();
@@ -102,29 +38,52 @@ async fn test_hatchet() {
     let my_task = hatchet.new_task(
         "step1",
         async move |input: SimpleInput,
-                    ctx: hatchet_sdk::Context|
+                    _ctx: hatchet_sdk::Context|
                     -> Result<SimpleOutput, MyError> {
-            ctx.log(String::from("Hello from Task 1!")).await?;
-            ctx.log(ctx.filter_payload().await?.to_string()).await?;
             Ok(SimpleOutput {
                 transformed_message: input.message.to_lowercase(),
             })
         },
     );
-    let workflow1 = hatchet
+    let mut workflow1 = hatchet
         .new_workflow::<SimpleInput, SimpleOutput>("rust-workflow3", vec![], vec![], vec![])
         .add_task(my_task)
         .unwrap();
 
-    Worker::new("rust-worker", hatchet.clone(), 5)
-        .unwrap()
-        .add_workflow(workflow1)
-        .start()
-        .await
-        .unwrap();
+    let workflow1_clone = workflow1.clone();
+    let worker_handle = tokio::spawn(async move {
+        Worker::new("rust-worker", hatchet.clone(), 5)
+            .unwrap()
+            .add_workflow(workflow1_clone)
+            .start()
+            .await
+            .unwrap()
+    });
+
+    // Give worker time to register task
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+    assert_eq!(
+        "uppercase",
+        workflow1
+            .run(
+                SimpleInput {
+                    message: "UPPERCASE".to_string()
+                },
+                None
+            )
+            .await
+            .unwrap()
+            .transformed_message
+    );
+    worker_handle.abort()
 }
 
-async fn start_containers_and_get_token() -> String {
+async fn start_containers_and_get_token() -> (
+    ContainerAsync<GenericImage>,
+    ContainerAsync<GenericImage>,
+    String,
+) {
     let postgres = GenericImage::new("postgres", "15.6")
         .with_exposed_port(5432.tcp())
         .with_cmd(vec!["postgres", "-c", "max_connections=200"])
@@ -132,8 +91,7 @@ async fn start_containers_and_get_token() -> String {
             Healthcheck::cmd(vec!["CMD-SHELL", "pg_isready -d hatchet -U hatchet"])
                 .with_interval(Some(std::time::Duration::from_secs(5)))
                 .with_timeout(Some(std::time::Duration::from_secs(5)))
-                .with_retries(Some(5))
-                .with_start_period(Some(std::time::Duration::from_secs(10))),
+                .with_retries(Some(5)),
         )
         .with_network("hatchet")
         .with_env_var("POSTGRES_USER", "hatchet")
@@ -147,6 +105,13 @@ async fn start_containers_and_get_token() -> String {
         .with_wait_for(WaitFor::message_on_either_std(
             "created tenant 707d0855-80ab-4e1f-a156-f1c4546cbf52",
         ))
+        .with_wait_for(WaitFor::Healthcheck(HealthWaitStrategy::new()))
+        .with_health_check(
+            Healthcheck::cmd(vec!["nc", "-z", "localhost", "7077"])
+                .with_interval(Some(std::time::Duration::from_secs(5)))
+                .with_timeout(Some(std::time::Duration::from_secs(5)))
+                .with_retries(Some(5)),
+        )
         .with_mapped_port(8888, 8888.tcp())
         .with_mapped_port(7077, 7077.tcp())
         .with_env_var(
@@ -186,5 +151,5 @@ async fn start_containers_and_get_token() -> String {
     let mut result = hatchet.exec(command).await.unwrap();
     let mut token = String::new();
     result.stdout().read_to_string(&mut token).await.unwrap();
-    return token;
+    (postgres, hatchet, token)
 }
