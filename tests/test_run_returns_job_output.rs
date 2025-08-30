42e89d64ec1c48787d5f8a906b36ba3357099e59
Ethan Wolinsky <ethan@alphataraxia.com>
2025-08-30 12:09:47 -0400

    Test DAG workflow

--- tests/test_run_returns_job_output.rs
+++ tests/test_run_returns_job_output.rs
@@ -147,7 +147,7 @@ async fn test_run_returns_error_if_job_fails() {
 }
 
 #[tokio::test]
-async fn test_spawn_child_workflow() {
+async fn test_dynamically_spawn_child_workflow() {
     let (_postgres, hatchet_container, token) = common::start_containers_and_get_token().await;
     let server_url = format!(
         "http://localhost:{}",
@@ -222,6 +222,79 @@ async fn test_spawn_child_workflow() {
 
     assert_eq!(
         "Hello from child task",
+        output.unwrap().get("output").unwrap()
+    );
+    worker_handle.abort()
+}
+
+#[tokio::test]
+async fn test_dag_workflow() {
+    let (_postgres, hatchet_container, token) = common::start_containers_and_get_token().await;
+    let server_url = format!(
+        "http://localhost:{}",
+        hatchet_container.get_host_port_ipv4(8888).await.unwrap()
+    );
+    let grpc_broadcast_address = format!(
+        "localhost:{}",
+        hatchet_container.get_host_port_ipv4(7077).await.unwrap()
+    );
+    let hatchet =
+        HatchetClient::from_token(&server_url, &grpc_broadcast_address, token.trim(), "none")
+            .await
+            .unwrap();
+
+    let parent_task = hatchet.new_task(
+        "parent_task",
+        async move |_input: hatchet_sdk::EmptyModel,
+                    _ctx: hatchet_sdk::Context|
+                    -> Result<serde_json::Value, MyError> {
+            Ok(serde_json::json!({"message": "I am your father"}))
+        },
+    );
+
+    let child_task = hatchet
+        .new_task(
+            "child_task",
+            async move |_input: hatchet_sdk::EmptyModel,
+                        ctx: hatchet_sdk::Context|
+                        -> Result<serde_json::Value, MyError> {
+                let parent_output = ctx.parent_output("parent_task").await.unwrap();
+                let message = parent_output.get("message").unwrap();
+                Ok(serde_json::json!({"output": format!("Parent said: {}", message.to_string())}))
+            },
+        )
+        .add_parent(&parent_task);
+
+    let mut dag_workflow = hatchet
+        .new_workflow::<hatchet_sdk::EmptyModel, serde_json::Value>(
+            "parent-workflow",
+            vec![],
+            vec![],
+            vec![],
+        )
+        .add_task(parent_task)
+        .unwrap()
+        .add_task(child_task)
+        .unwrap();
+
+    let dag_workflow_clone = dag_workflow.clone();
+    let worker_handle = tokio::spawn(async move {
+        Worker::new("rust-worker", hatchet.clone(), 5)
+            .unwrap()
+            .add_workflow(dag_workflow_clone)
+            .start()
+            .await
+            .unwrap()
+    });
+
+    // Give worker time to register task
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+    let output = dag_workflow.run(hatchet_sdk::EmptyModel, None).await;
+    println!("{:?}", output);
+
+    assert_eq!(
+        "Parent said: \"I am your father\"",
         output.unwrap().get("output").unwrap()
     );
     worker_handle.abort()
