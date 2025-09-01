6fc86622c9457152548c477296a4f438a4d73e16
Ethan Wolinsky <ethan@alphataraxia.com>
2025-09-01 09:32:07 -0400

    Add simple task example

---
+++ examples/simple.rs
@@ -0,0 +1,47 @@
+use anyhow;
+use hatchet_sdk::{Context, Hatchet};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+    let hatchet = Hatchet::from_env().await.unwrap();
+
+    #[derive(Serialize, Deserialize)]
+    struct SimpleInput {
+        message: String,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    struct SimpleOutput {
+        message: String,
+    }
+
+    let task = hatchet.task(
+        "simple-task",
+        async move |input: SimpleInput, ctx: Context| -> anyhow::Result<SimpleOutput> {
+            ctx.log("Starting simple task").await?;
+            Ok(SimpleOutput {
+                message: input.message.to_lowercase(),
+            })
+        },
+    );
+
+    let mut workflow = hatchet
+        .workflow::<SimpleInput, SimpleOutput>()
+        .name(String::from("simple-workflow"))
+        .build()
+        .unwrap()
+        .add_task(task)
+        .unwrap();
+
+    workflow
+        .run_no_wait(
+            SimpleInput {
+                message: String::from("Hello, world!"),
+            },
+            None,
+        )
+        .await
+        .unwrap();
+}
